e4ca80c0d1142674ce2536f61aa31a71a62ff0ae
jakedwk <dwk975@163.com>
2015-10-18 20:29:27 +0800

    20151018_2029 1.修改二值化阈值、膨胀次数、轮廓的大小等参数 2.修改背景重新获取方法与其阈值

--- dtmove.py
+++ dtmove.py
@@ -7,62 +7,55 @@ def dtmove(cap = cv2.VideoCapture(0)):
     occflag = 0
     motionCounter = 0
     avg = None
+    times = 0
     time.sleep(1)
     for i in range(0,50):
         ret,frame = cap.read()
     ret,frame = cap.read()
-    grabold = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    print "[INFO] starting background model..."
-    avg = cv2.GaussianBlur(grabold, (21, 21), 0)
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    avg = cv2.GaussianBlur(gray, (21, 21), 0)
+    grabold = avg.copy()
+
     while True:
-        if cv2.waitKey(10)&0xFF == ord(' '):
-            while True:
-                if cv2.waitKey(10)&0xFF == ord(' '):
-                    break
         ret,frame = cap.read()
         timestamp = datetime.datetime.now()
         grab = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
         gray = cv2.absdiff(grab, cv2.convertScaleAbs(avg))
         gray = cv2.GaussianBlur(gray, (21, 21), 0)
-        ret, thresh = cv2.threshold(gray, 55, 255, cv2.THRESH_BINARY)
-        thresh = cv2.dilate(thresh, None, iterations=2)
+        ret, thresh = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY)
+        thresh = cv2.dilate(thresh, None, iterations=5)
         (cnts,_) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-        # print cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-        cv2.waitKey(5)
-
         for c in cnts:
-            # if the contour is too small, ignore it
-            if cv2.contourArea(c) < 1000:
+            if cv2.contourArea(c) < 500:
                 continue
-            # compute the bounding box for the contour, draw it on the frame,
-            # and update the text
             # 计算轮廓的边界框，在当前帧中画出该框
             (x, y, w, h) = cv2.boundingRect(c)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            # 计算背景变化程度重新选取背景
             occflag = 1
-            diflx = cv2.absdiff(grab, cv2.convertScaleAbs(grabold))
-            (difzx,_,_,_) = cv2.mean(diflx)
-            if difzx<4 :  #背景重新获取
-                # print "[INFO] starting background model..."
-                avg = cv2.GaussianBlur(grab, (21, 21), 0)
+
         #背景重新获取
         if (occflag == 0):    
             (difavg,_,_,_) = cv2.mean(gray)
-            diflx = cv2.absdiff(grab, cv2.convertScaleAbs(grabold))
-            (difzx,_,_,_) = cv2.mean(diflx)
-            print difavg,difzx
+            print 'mean',difavg
             if(difavg >7):
-            # if(difavg >9)|(difzx < 4):
-                # print "[INFO] starting background model..."
                 avg = cv2.GaussianBlur(grab, (21, 21), 0)
+        else:
+            if times >= 30:
+                times = 0
+                diflx = cv2.absdiff(grab, cv2.convertScaleAbs(grabold))
+                (difzx,_,_,_) = cv2.mean(diflx)
+                print 'diff',diflx
+                grabold = avg.copy()
+                if difzx<3 :  
+                    avg = cv2.GaussianBlur(grab, (21, 21), 0)
+            times=times + 1
 
         occflag = 0
         grabold = grab.copy()
         cv2.imshow('farme',frame)
         cv2.imshow('thresh',thresh)
         cv2.imshow('gray',gray)
-        cv2.imshow('difxx',diflx)
+
         if cv2.waitKey(1)&0xFF == ord('s'):
             ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
             cv2.imwrite("/home/lucky_d/pyex/images/"+ts+'.jpg',frame)
