1481f30ffca2c6693261a2c9c0ae3ba3308f893b
jakedwk <dwk975@163.com>
2015-10-19 10:57:04 +0800

    20151019_1057 发现高斯模糊的位置错误，已改正。

--- dtmove.py
+++ dtmove.py
@@ -17,13 +17,13 @@ def dtmove(cap = cv2.VideoCapture(0)):
     grabold = avg.copy()
 
     while True:
-        ret,frame = cap.read()
         timestamp = datetime.datetime.now()
+        ret,frame = cap.read()
         grab = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        gray = cv2.absdiff(grab, cv2.convertScaleAbs(avg))
-        gray = cv2.GaussianBlur(gray, (21, 21), 0)
-        ret, thresh = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY)
-        thresh = cv2.dilate(thresh, None, iterations=5)
+        gray = cv2.GaussianBlur(grab, (21, 21), 0)
+        differ = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
+        ret, thresh = cv2.threshold(differ, 50, 255, cv2.THRESH_BINARY)
+        thresh = cv2.dilate(thresh, None, iterations=11)
         (cnts,_) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
         for c in cnts:
             if cv2.contourArea(c) < 500:
@@ -35,19 +35,19 @@ def dtmove(cap = cv2.VideoCapture(0)):
 
         #背景重新获取
         if (occflag == 0):    
-            (difavg,_,_,_) = cv2.mean(gray)
-            print 'mean',difavg
-            if(difavg >7):
-                avg = cv2.GaussianBlur(grab, (21, 21), 0)
+            (difavg,_,_,_) = cv2.mean(differ)
+            print 'm', difavg
+            if(difavg >2):
+                avg = gray.copy()
         else:
             if times >= 30:
                 times = 0
                 diflx = cv2.absdiff(grab, cv2.convertScaleAbs(grabold))
                 (difzx,_,_,_) = cv2.mean(diflx)
-                print 'diff',diflx
+                print 'd',difzx
                 grabold = avg.copy()
-                if difzx<3 :  
-                    avg = cv2.GaussianBlur(grab, (21, 21), 0)
+                if difzx<2 :  
+                    avg = gray.copy()
             times=times + 1
 
         occflag = 0
